43ce2b1bf8c9d56fda153ca6d3b494e184d2eeb6
Bojidar Dermendjiev <bojidardermendjiew@abv.bg>
2023-06-30 14:49:48 +0300

    push

--- 11. Exam Preparations/06. Algorithms Fundamentals with C# - Exam - 09 Jan 2021/02. Paths/StartUp.cs
+++ 11. Exam Preparations/06. Algorithms Fundamentals with C# - Exam - 09 Jan 2021/02. Paths/StartUp.cs
@@ -7,38 +7,32 @@
     {
         static void Main()
         {
-            int n = int.Parse(Console.ReadLine());
-            List<int>[] adj = new List<int>[n];
-            for (int i = 0; i < n; i++)
+            int nodes = int.Parse(Console.ReadLine());
+            List<int>[] graph = new List<int>[nodes];
+            ;
+            for (int currentNode = 0; currentNode < nodes; currentNode++)
             {
                 string[] children = Console.ReadLine().Split(' ');
-                adj[i] = new List<int>();
+                graph[currentNode] = new List<int>();
+
                 foreach (string child in children)
-                {
                     if (!string.IsNullOrEmpty(child))
-                        adj[i].Add(int.Parse(child));
-                }
+                        graph[currentNode].Add(int.Parse(child));
+
             }
-            for (int i = 0; i < n - 1; i++)
-            {
-                DFS(adj, i, new List<int>());
-            }
+            for (int startedNote = 0; startedNote < nodes - 1; startedNote++)
+                DFS(graph, startedNote, new List<int>());
         }
 
-        private static void DFS(List<int>[] adj, int node, List<int> path)
+        private static void DFS(List<int>[] graph, int node, List<int> path)
         {
             path.Add(node);
-            if (node == adj.Length - 1)
-            {
+            if (node == graph.Length - 1)
                 Console.WriteLine(string.Join(" ", path));
-            }
             else
-            {
-                foreach (int child in adj[node])
-                {
-                    DFS(adj, child, path);
-                }
-            }
+                foreach (int child in graph[node])
+                    DFS(graph, child, path);
+
             path.RemoveAt(path.Count - 1);
         }
     }
