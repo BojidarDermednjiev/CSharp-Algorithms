e18f09b84b8e4976691bbfb50721d9cbe28d4c20
Bojidar Dermendjiev <bojidardermendjiew@abv.bg>
2023-07-21 10:04:06 +0300

    Upload solutions for 08. Algorithms Fundamentals with C# - Retake Exam - 08 July 2023

---
+++ 11. Exam Preparations/08. Algorithms Fundamentals with C# - Retake Exam - 08 July 2023/02. Crypto Exchange/StartUp.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Dictionary<string, List<string>> tradeRules = new Dictionary<string, List<string>>();
+
+        // Read the supported pairs and build the trade rules dictionary
+        for (int i = 0; i < n; i++)
+        {
+            string[] pair = Console.ReadLine().Split(" - ");
+            string asset1 = pair[0];
+            string asset2 = pair[1];
+
+            if (!tradeRules.ContainsKey(asset1))
+            {
+                tradeRules[asset1] = new List<string>();
+            }
+
+            tradeRules[asset1].Add(asset2);
+
+            if (!tradeRules.ContainsKey(asset2))
+            {
+                tradeRules[asset2] = new List<string>();
+            }
+
+            tradeRules[asset2].Add(asset1);
+        }
+
+        string[] request = Console.ReadLine().Split(" -> ");
+        string source = request[0];
+        string target = request[1];
+
+        int minSwaps = FindMinSwaps(tradeRules, source, target);
+        Console.WriteLine(minSwaps == int.MaxValue ? -1 : minSwaps);
+    }
+
+    static int FindMinSwaps(Dictionary<string, List<string>> tradeRules, string source, string target)
+    {
+        Queue<string> queue = new Queue<string>();
+        HashSet<string> visited = new HashSet<string>();
+        Dictionary<string, int> swaps = new Dictionary<string, int>();
+
+        queue.Enqueue(source);
+        swaps[source] = 0;
+
+        while (queue.Count > 0)
+        {
+            string current = queue.Dequeue();
+
+            if (current == target)
+            {
+                return swaps[current];
+            }
+
+            visited.Add(current);
+
+            if (tradeRules.ContainsKey(current))
+            {
+                foreach (string asset in tradeRules[current])
+                {
+                    if (!visited.Contains(asset))
+                    {
+                        queue.Enqueue(asset);
+                        swaps[asset] = swaps[current] + 1;
+                    }
+                }
+            }
+        }
+
+        return int.MaxValue;
+    }
+}
---
+++ 11. Exam Preparations/08. Algorithms Fundamentals with C# - Retake Exam - 08 July 2023/03. Contaminated Path/StartUp.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Cell
+{
+    public int Row { get; set; }
+    public int Col { get; set; }
+    public int Fertility { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int gridSize = int.Parse(Console.ReadLine());
+        List<List<Cell>> grid = new List<List<Cell>>();
+
+        // Read the grid elements
+        for (int i = 0; i < gridSize; i++)
+        {
+            string[] rowElements = Console.ReadLine().Split(' ');
+            List<Cell> row = new List<Cell>();
+
+            for (int j = 0; j < gridSize; j++)
+            {
+                int fertility = int.Parse(rowElements[j]);
+                Cell cell = new Cell { Row = i, Col = j, Fertility = fertility };
+                row.Add(cell);
+            }
+
+            grid.Add(row);
+        }
+
+        // Read the contaminated cells
+        string[] contaminatedCells = Console.ReadLine().Split(' ');
+        HashSet<string> contaminatedSet = new HashSet<string>(contaminatedCells);
+
+        int maxFertility = FindMaxFertility(grid, contaminatedSet, gridSize);
+
+        Console.WriteLine($"Max total fertility: {maxFertility}");
+        Console.WriteLine($"Best path: {FindBestPath(grid, gridSize)}");
+    }
+
+    static int FindMaxFertility(List<List<Cell>> grid, HashSet<string> contaminatedSet, int gridSize)
+    {
+        int[,] dp = new int[gridSize, gridSize];
+
+        // Initialize the first row and first column
+        dp[0, 0] = grid[0][0].Fertility;
+        for (int i = 1; i < gridSize; i++)
+        {
+            // If a cell is contaminated, set its fertility to 0
+            dp[0, i] = dp[0, i - 1] + (contaminatedSet.Contains($"0,{i}") ? 0 : grid[0][i].Fertility);
+            dp[i, 0] = dp[i - 1, 0] + (contaminatedSet.Contains($"{i},0") ? 0 : grid[i][0].Fertility);
+        }
+
+        // Compute the maximum fertility for each cell
+        for (int i = 1; i < gridSize; i++)
+        {
+            for (int j = 1; j < gridSize; j++)
+            {
+                if (contaminatedSet.Contains($"{i},{j}"))
+                {
+                    dp[i, j] = 0;
+                }
+                else
+                {
+                    dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]) + grid[i][j].Fertility;
+                }
+            }
+        }
+
+        return dp[gridSize - 1, gridSize - 1];
+    }
+
+    static string FindBestPath(List<List<Cell>> grid, int gridSize)
+    {
+        int i = gridSize - 1;
+        int j = gridSize - 1;
+        string path = $"({i}, {j})";
+
+        while (i > 0 || j > 0)
+        {
+            if (i > 0 && j > 0 && grid[i - 1][j].Fertility > grid[i][j - 1].Fertility)
+            {
+                i--;
+            }
+            else if (i > 0 && j > 0 && grid[i - 1][j].Fertility < grid[i][j - 1].Fertility)
+            {
+                j--;
+            }
+            else if (i > 0)
+            {
+                i--;
+            }
+            else
+            {
+                j--;
+            }
+
+            path = $"({i}, {j}) " + path;
+        }
+
+        return "[" + path + "]";
+    }
+}
